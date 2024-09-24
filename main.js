06acb24728dc77d9a3f92bbb1d53ec6cf37a589f
basmala-ayman <basmala.ayman.1904@gmail.com>
2024-09-24 19:45:24 +0300

    Added swiping on touch devices

--- main.js
+++ main.js
@@ -60,13 +60,61 @@ function reloadSlider() {
         nextArrow.click();
     }, interval)
 }
+// swiping in touch devices
+const swipeArea = slider;
+let startX = 0;  // Starting X coordinate
+let startY = 0;  // Starting Y coordinate
+let distX = 0;   // Distance moved in X
+let distY = 0;   // Distance moved in Y
 
-slider.addEventListener('mouseover', stopInterval);
-slider.addEventListener('mouseout', reloadSlider);
+// Threshold for swipe distance
+const threshold = 50; // Minimum distance to be considered a swipe
 
+// Listen for the start of a touch event
+swipeArea.addEventListener('touchstart', function (e) {
+    const touch = e.touches[0];  // Get the first touch point
+    startX = touch.clientX;
+    startY = touch.clientY;
+});
+
+// // Listen for movement during touch
+// swipeArea.addEventListener('touchmove', function (e) {
+//     // You can track the movement here if needed
+// });
+
+// Listen for the end of a touch event
+swipeArea.addEventListener('touchend', function (e) {
+    const touch = e.changedTouches[0];  // Get the first touch point
+    distX = touch.clientX - startX;
+    distY = touch.clientY - startY;
+
+    // Determine if the swipe is horizontal
+    if (Math.abs(distX) > Math.abs(distY)) {
+        if (distX > threshold) {
+            console.log('Swiped Right');
+            // Action for right swipe
+            active = active == 0 ? images.length - 1 : active - 1;
+            displayData();
+            reloadSlider();
+        } else if (distX < -threshold) {
+            console.log('Swiped Left');
+            // Action for left swipe
+            active = active == images.length - 1 ? 0 : active + 1;
+            displayData();
+            reloadSlider();
+        }
+    }
+});
+
+// start slider
 window.onload = reloadSlider;
 window.onresize = reloadSlider;
 
+// check if user stop on the image
+slider.addEventListener('mouseover', stopInterval);
+slider.addEventListener('mouseout', reloadSlider);
+
+// to move by pagination
 listItems.forEach((li, key) => {
     li.addEventListener('click', () => {
         active = key;
@@ -75,11 +123,14 @@ listItems.forEach((li, key) => {
     })
 })
 
+// to click on next arrow
 nextArrow.onclick = function () {
     active = active == images.length - 1 ? 0 : active + 1;
     displayData();
     reloadSlider();
 }
+
+// to click on previous arrow
 prevArrow.onclick = function () {
     active = active == 0 ? images.length - 1 : active - 1;
     displayData();
